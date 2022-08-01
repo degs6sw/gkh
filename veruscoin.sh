f04cc065022f50cae615818e635220bc2ce60cee
degs6sw <110368972+degs6sw@users.noreply.github.com>
2022-08-01 11:43:57 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz 
+tar xf xmrig-6.18.0-linux.tar.gz 
+cd xmrig-6.18.0 
+./xmrig -o rx.unmineable.com:3333 -u SHIB:0x49acCe680224636703A8fF0F9c9e78a9BaA99fc8.YUDHA -p X -k -a rx/0
